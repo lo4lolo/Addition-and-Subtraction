e9e9adf99286ed597f47e9ba4d2f543bacdb969d
lo4lolo <4lolo@naver.com>
2025-09-15 03:35:39 +0900

    Fix: Add homepage and base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
+    base: '/Addition-and-Subtraction/'
     const env = loadEnv(mode, '.', '');
     return {
       define: {
